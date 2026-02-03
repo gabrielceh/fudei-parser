41196b1b6d06fae87b3671e2a63b27f3ba7c765d
gabrielceh <gabrielcehu@gmail.com>
2026-02-03 09:36:07 -0500

    alias para los test

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,29 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1"
+  },
+  roots: [
+    "<rootDir>/test",
+    "<rootDir>/src"
+  ],
+  testMatch: [
+    "**/*.spec.ts",
+    "**/*.test.ts"
+  ],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json"
+    }
+  },
+
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+
+  clearMocks: true
 };
